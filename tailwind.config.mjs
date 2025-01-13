d6c9ac5a3743afeee74bff2a0afbc435c7cb8d32
Lucas Andrade <lucasantos.dev@gmail.com>
2025-01-13 16:25:06 -0300

    Add tailwind config and styles in Layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: '#B443DA',
-				secondary: '#FFC751',
-			},
-			fontFamily: {
-				lato: ['Lato', 'serif'],
-				oswald: ['Oswald', 'serif'],
-			},
-		},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#B443DA',
+                secondary: '#FFC751',
+                overlay: 'rgba(0, 0, 0, .7)',
+            },
+            fontFamily: {
+                lato: ['Lato', 'serif'],
+                oswald: ['Oswald', 'serif'],
+            },
+            padding: {
+                22: '5.5rem',
+            },
+        },
+    },
+    plugins: [],
 }
